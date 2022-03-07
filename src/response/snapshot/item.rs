6e0f93a249a4fb16018ecf9c9683521f41b767a3
juliarose <julialunarose@gmail.com>
2022-03-07 15:51:32 -0500

    changed snapshot listing getters for item to be bound to item

--- src/response/snapshot/item.rs
+++ src/response/snapshot/item.rs
@@ -1,10 +1,10 @@
 use serde::{Serialize, Deserialize};
-use tf2_enum::Quality;
-use crate::response::attributes::{Attributes};
+use crate::response::attributes::{Attributes, Value as AttributeValue};
 use crate::response::deserializers::{
     deserialize_attributes,
     from_optional_number_or_string
 };
+use tf2_enum::{Wear, Quality, KillstreakTier};
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub struct Item {
@@ -25,4 +25,63 @@ pub struct Item {
     #[serde(default)]
     #[serde(deserialize_with = "deserialize_attributes")]
     pub attributes: Attributes,
+}
+
+impl Item {
+
+    pub fn get_quality(&self) -> Quality {
+        self.quality.clone()
+    }
+    
+    pub fn get_particle_value(&self) -> Option<u32> {
+        if let Some(attribute) = self.attributes.get(&134) {
+            if let Some(float_value) = &attribute.float_value {
+                return Some(*float_value as u32);
+            }
+        }
+        
+        None
+    }
+    
+    pub fn get_skin_value(&self) -> Option<u32> {
+        if let Some(attribute) = self.attributes.get(&834) {
+            if let Some(value) = &attribute.value {
+                if let AttributeValue::Number(value) = value {
+                    return Some(*value as u32);
+                }
+            }
+        }
+        
+        None
+    }
+    
+    pub fn get_killstreak_tier(&self) -> Option<KillstreakTier> {
+        if let Some(attribute) = self.attributes.get(&2025) {
+            if let Some(float_value) = &attribute.float_value {
+                if let Ok(killstreak_tier) = KillstreakTier::try_from(*float_value as u8) {
+                    return Some(killstreak_tier);
+                }
+            }
+        }
+        
+        None
+    }
+    
+    pub fn get_wear(&self) -> Option<Wear> {
+        if let Some(attribute) = self.attributes.get(&725) {
+            if let Some(float_value) = &attribute.float_value {
+                let wear_value = (float_value * 5.0).round() as u8;
+                
+                if let Ok(wear) = Wear::try_from(wear_value) {
+                    return Some(wear);
+                }
+            }
+        }
+        
+        None
+    }
+    
+    pub fn is_festive(&self) -> bool {
+        self.attributes.contains_key(&2053)
+    }
 }
--- src/response/snapshot/listing.rs
+++ src/response/snapshot/listing.rs
@@ -2,7 +2,6 @@ use serde::{Serialize, Deserialize};
 use crate::{
     ListingIntent,
     time::ServerTime,
-    response::attributes::Value as AttributeValue,
     response::deserializers::{
         bool_from_int,
         listing_intent_enum_from_str
@@ -10,7 +9,6 @@ use crate::{
 };
 use super::Item;
 use tf2_price::Currencies;
-use tf2_enum::{Wear, Quality, KillstreakTier};
 use chrono::serde::ts_seconds;
 use steamid_ng::SteamID;
 
@@ -41,61 +39,5 @@ impl Listing {
     
     pub fn is_sell_order(&self) -> bool {
         self.intent == ListingIntent::Sell
-    }
-    
-    pub fn get_quality(&self) -> Quality {
-        self.item.quality.clone()
-    }
-    
-    pub fn get_particle_value(&self) -> Option<u32> {
-        if let Some(attribute) = self.item.attributes.get(&134) {
-            if let Some(float_value) = &attribute.float_value {
-                return Some(*float_value as u32);
-            }
-        }
-        
-        None
-    }
-    
-    pub fn get_skin_value(&self) -> Option<u32> {
-        if let Some(attribute) = self.item.attributes.get(&834) {
-            if let Some(value) = &attribute.value {
-                if let AttributeValue::Number(value) = value {
-                    return Some(*value as u32);
-                }
-            }
-        }
-        
-        None
-    }
-    
-    pub fn get_killstreak_tier(&self) -> Option<KillstreakTier> {
-        if let Some(attribute) = self.item.attributes.get(&2025) {
-            if let Some(float_value) = &attribute.float_value {
-                if let Ok(killstreak_tier) = KillstreakTier::try_from(*float_value as u8) {
-                    return Some(killstreak_tier);
-                }
-            }
-        }
-        
-        None
-    }
-    
-    pub fn get_wear(&self) -> Option<Wear> {
-        if let Some(attribute) = self.item.attributes.get(&725) {
-            if let Some(float_value) = &attribute.float_value {
-                let wear_value = (float_value * 5.0).round() as u8;
-                
-                if let Ok(wear) = Wear::try_from(wear_value) {
-                    return Some(wear);
-                }
-            }
-        }
-        
-        None
-    }
-    
-    pub fn is_festive(&self) -> bool {
-        self.item.attributes.contains_key(&2053)
     }
 }
