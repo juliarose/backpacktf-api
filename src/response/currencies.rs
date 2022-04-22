ff6fd2de30c068bca47ad7b340cd5481d3514280
juliarose <julialunarose@gmail.com>
2022-04-22 01:22:36 -0400

    fmt::Display on response Currencies

--- src/response/currencies.rs
+++ src/response/currencies.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 pub use tf2_price::{ListingCurrencies, USDCurrencies};
+use std::fmt;
 
 #[derive(Deserialize, Serialize, PartialEq, Clone, Debug)]
 #[serde(untagged)]
@@ -8,11 +9,41 @@ pub enum Currencies {
     Cash(USDCurrencies)
 }
 
+impl fmt::Display for Currencies {
+    
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Currencies::InGame(currencies) => write!(f, "{}", currencies),
+            Currencies::Cash(currencies) => write!(f, "{}", currencies),
+        }
+    }
+}
+
 impl Currencies {
     
     pub fn is_in_game(&self) -> bool {
         match self {
             Currencies::InGame(_) => true,
+            _ => false,
+        }
+    }
+}
+
+impl PartialEq<ListingCurrencies> for Currencies {
+    
+    fn eq(&self, other: &ListingCurrencies) -> bool {
+        match self {
+            Currencies::InGame(currencies) => currencies == other,
+            _ => false,
+        }
+    }
+}
+
+impl PartialEq<tf2_price::Currencies> for Currencies {
+    
+    fn eq(&self, other: &tf2_price::Currencies) -> bool {
+        match self {
+            Currencies::InGame(currencies) => currencies == other,
             _ => false,
         }
     }
