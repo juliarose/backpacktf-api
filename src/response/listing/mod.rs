d74a9fccf065b03717bc05438baa8c6400fe3144
juliarose <julialunarose@gmail.com>
2022-04-27 07:51:21 -0400

    added benchmarks

--- src/response/listing/mod.rs
+++ src/response/listing/mod.rs
@@ -1,7 +1,6 @@
 mod batch_limit;
 mod user;
 mod user_agent;
-mod summary;
 mod value;
 mod item;
 pub mod attributes;
@@ -11,7 +10,6 @@ pub mod delete_listing;
 pub mod update_listing;
 pub use batch_limit::BatchLimit;
 pub use user::{User, UserBan};
-pub use summary::Summary;
 pub use item::Item;
 pub use value::Value;
 pub use user_agent::UserAgent;
--- src/response/listing/summary.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct Summary {
-    pub value: String,
-}
