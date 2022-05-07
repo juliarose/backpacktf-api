a74131b41aacf17a3d20211f575f7f8f1d044e50
juliarose <julialunarose@gmail.com>
2022-05-07 18:21:04 -0400

    refined players/bans models

--- src/response/player.rs
+++ src/response/player.rs
@@ -2,6 +2,9 @@ use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
 use crate::{SteamID, time::ServerTime};
 use chrono::serde::ts_seconds_option;
+use super::deserializers::string_or_number;
+use num_enum::{TryFromPrimitive, IntoPrimitive};
+use serde_repr::{Serialize_repr, Deserialize_repr};
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub struct Trust {
@@ -11,6 +14,7 @@ pub struct Trust {
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub struct Ban {
+    #[serde(deserialize_with = "string_or_number")]
     pub r#type: u32,
     pub reason: String,
     #[serde(default)]
@@ -36,8 +40,30 @@ pub struct Player {
     pub name: String,
     pub backpack_value: HashMap<u32, f32>,
     pub backpack_tf_banned: Option<Ban>,
+    pub backpack_tf_bans: Option<HashMap<String, Ban>>,
     pub backpack_tf_reputation: i32,
     #[serde(default)]
     pub backpack_tf_group: bool,
-    pub backpack_tf_trust: Trust
+    pub backpack_tf_trust: Trust,
+    #[serde(default)]
+    pub steamrep_scammer: bool,
+}
+
+#[derive(Serialize_repr, Deserialize_repr, Hash, Eq, PartialEq, Clone, Debug, TryFromPrimitive, IntoPrimitive)]
+#[repr(u8)]
+pub enum BanType {
+    AllFeatures = 1,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn parses_ban() {
+        let player: Player = serde_json::from_str(include_str!("fixtures/player.json")).unwrap();
+        
+        assert_eq!(player.backpack_tf_banned.unwrap().permabanned(), true);
+        assert_eq!(player.steamrep_scammer, true);
+    }
 }
