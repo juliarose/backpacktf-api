5364e11e94fd26f12282f0c138b2b3aabd5efc91
juliarose <julialunarose@gmail.com>
2022-10-13 15:04:28 -0400

    changed archived listing example

---
+++ examples/delete_archived_listings.rs
@@ -0,0 +1,28 @@
+use backpacktf_api::{
+    BackpackAPI,
+    error::Error,
+};
+use dotenv::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    dotenv().ok();
+    
+    let backpacktf = BackpackAPI::builder()
+        .key(&env::var("KEY").unwrap())
+        .token(&env::var("TOKEN").unwrap())
+        .build();
+    let (
+        archived_listings,
+        _cursor,
+    ) = backpacktf.get_archived_listings(0, 1).await?;
+    
+    if let Some(archived_listing) = archived_listings.into_iter().next() {
+        backpacktf.delete_archived_listing(&archived_listing.id).await?;
+    } else {
+        println!("No archived listings");
+    }
+    
+    Ok(())
+}
